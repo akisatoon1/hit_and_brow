c528bd8b2bf6454881624e03f469feede55a3773
akisatoon <akisatoon@gmail.com>
2025-07-10 16:59:31 +0900

    refact: format

--- model.cpp
+++ model.cpp
@@ -11,7 +11,8 @@ Num::Num(int d1_, int d2_, int d3_)
     : d1(d1_), d2(d2_), d3(d3_) {}
 
 // 3桁の数字として有効か判定
-bool Num::valid() const {
+bool Num::valid() const
+{
     // 各桁が0-9の範囲であることを確認
     bool c1 = (0 <= d1 && d1 <= 9);
     bool c2 = (0 <= d2 && d2 <= 9);
@@ -24,31 +25,40 @@ bool Num::valid() const {
 }
 
 // guessとの位置一致数（ヒット数）
-int Num::hit(const Num &guess) const {
+int Num::hit(const Num &guess) const
+{
     int cnt = 0;
-    if (d1 == guess.d1) cnt++;
-    if (d2 == guess.d2) cnt++;
-    if (d3 == guess.d3) cnt++;
+    if (d1 == guess.d1)
+        cnt++;
+    if (d2 == guess.d2)
+        cnt++;
+    if (d3 == guess.d3)
+        cnt++;
     return cnt;
 }
 
 // guessとの数字一致数（位置違い、ブロー数）
-int Num::brow(const Num &guess) const {
+int Num::brow(const Num &guess) const
+{
     int cnt = 0;
-    if (d1 == guess.d2 || d1 == guess.d3) cnt++;
-    if (d2 == guess.d1 || d2 == guess.d3) cnt++;
-    if (d3 == guess.d1 || d3 == guess.d2) cnt++;
+    if (d1 == guess.d2 || d1 == guess.d3)
+        cnt++;
+    if (d2 == guess.d1 || d2 == guess.d3)
+        cnt++;
+    if (d3 == guess.d1 || d3 == guess.d2)
+        cnt++;
     return cnt;
 }
 
 // ==================== Cond ====================
 
 // guess, hit, browを指定するコンストラクタ
-Cond::Cond(const Num& guess_, int hit_, int brow_)
+Cond::Cond(const Num &guess_, int hit_, int brow_)
     : guess(guess_), hit(hit_), brow(brow_) {}
 
 // 出力用ストリーム演算子
-std::ostream& operator<<(std::ostream& out, const Num& num) {
+std::ostream &operator<<(std::ostream &out, const Num &num)
+{
     out << num.d1
         << num.d2
         << num.d3;
@@ -56,15 +66,18 @@ std::ostream& operator<<(std::ostream& out, const Num& num) {
 }
 
 // hit, browが有効範囲か判定
-bool Cond::valid() const {
+bool Cond::valid() const
+{
     bool h = (0 <= hit && hit <= 3);
     bool b = (0 <= brow && brow <= 3);
     return h && b;
 }
 
 // nがこの条件(guess, hit, brow)を満たすか判定
-bool Cond::filter(const Num& n) const {
-    if (!guess.valid()) return false;
+bool Cond::filter(const Num &n) const
+{
+    if (!guess.valid())
+        return false;
     bool b1 = (hit == n.hit(guess));
     bool b2 = (brow == n.brow(guess));
     return b1 && b2;
--- model.h
+++ model.h
@@ -4,34 +4,35 @@
 #include <iostream>
 
 // ==================== Num ====================
-struct Num {
+struct Num
+{
     int d1, d2, d3; ///< 各桁の数字: d1d2d3で表される
 
     // コンストラクタ
-    Num(int n);                ///< 3桁のintから初期化
+    Num(int n);                  ///< 3桁のintから初期化
     Num(int d1, int d2, int d3); ///< 各桁から初期化
 
     // メンバ関数
-    bool valid() const;            ///< 有効な3桁か
-    int hit(const Num &guess) const;   ///< ヒット数
-    int brow(const Num &guess) const;  ///< ブロー数
+    bool valid() const;               ///< 有効な3桁か
+    int hit(const Num &guess) const;  ///< ヒット数
+    int brow(const Num &guess) const; ///< ブロー数
 };
 
-
 // ==================== Cond ====================
-struct Cond {
-    Num guess; ///< 推測値
+struct Cond
+{
+    Num guess;     ///< 推測値
     int hit, brow; ///< ヒット数・ブロー数
 
     // コンストラクタ
-    Cond(const Num& guess, int hit, int brow);
+    Cond(const Num &guess, int hit, int brow);
 
     // メンバ関数
-    bool valid() const; ///< hit, browが有効範囲か
-    bool filter(const Num& n) const; ///< この条件でnが成立するか
+    bool valid() const;              ///< hit, browが有効範囲か
+    bool filter(const Num &n) const; ///< この条件でnが成立するか
 };
 
 // 出力用ストリーム演算子
-std::ostream& operator<<(std::ostream& os, const Num& num);
+std::ostream &operator<<(std::ostream &os, const Num &num);
 
 #endif // MODEL_H
